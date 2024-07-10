5537d5ffa0c11719f1a79a917e267cea68d0fdc4
oguz.topcu <oguz.topcu@antalya.edu.tr>
2024-07-10 10:54:33 +0300

    update

--- database/migrations/2024_06_10_135216_create_departments_table.php
+++ database/migrations/2024_06_10_135216_create_departments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->string('faculty');
+            $table->string('faculty')->nullable();
             $table->string('name');
             $table->double('annual_fee')->default(0);
             $table->double('discounted_fee')->default(0);
