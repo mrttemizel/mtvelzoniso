27ab440e8b5b61c9e3f15ddca2840ab08d7b5157
oguz.topcu <oguztopcu7@gmail.com>
2024-07-31 14:05:24 +0300

    update

--- database/seeders/DepartmentsTableSeeder.php
+++ database/seeders/DepartmentsTableSeeder.php
@@ -15,97 +15,93 @@ class DepartmentsTableSeeder extends Seeder
     {
         $departments = array(
             "FACULTY OF DENTISTRY" => array(
-                "Dentistry (Turkish)" => 11900
+                "Dentistry (Turkish)" => array("price" => 14000, "discounted_price" => 14000)
             ),
             "FACULTY OF ENGINEERING AND NATURAL SCIENCES" => array(
-                "Computer Engineering (English)" => 3320,
-                "Electrical and Electronics Engineering (English)" => 3320,
-                "Industrial Engineering (English)" => 3320,
-                "Mechanical Engineering (English)" => 3320,
-                "Civil Engineering (English)" => 3320
+                "Computer Engineering (English)" => array("price" => 8300, "discounted_price" => 3735),
+                "Electrical and Electronics Engineering (English)" => array("price" => 8300, "discounted_price" => 3735),
+                "Industrial Engineering (English)" => array("price" => 8300, "discounted_price" => 3735),
+                "Mechanical Engineering (English)" => array("price" => 8300, "discounted_price" => 3735),
+                "Civil Engineering (English)" => array("price" => 8300, "discounted_price" => 3735)
             ),
             "FACULTY OF FINE ARTS AND ARCHITECTURE" => array(
-                "Architecture (English)" => 3320,
-                "Interior Architecture and Environmental Design (English)" => 3320
+                "Architecture (English)" => array("price" => 8300, "discounted_price" => 3735),
+                "Interior Architecture and Environmental Design (English)" => array("price" => 8300, "discounted_price" => 3735)
             ),
             "FACULTY OF TOURISM" => array(
-                "Tourism Management (English)" => 2905,
-                "Gastronomy and Culinary Arts (English)" => 2905
+                "Tourism Management (English)" => array("price" => 8300, "discounted_price" => 3320),
+                "Gastronomy and Culinary Arts (English)" => array("price" => 8300, "discounted_price" => 3320)
             ),
             "FACULTY OF BUSINESS AND SOCIAL SCIENCES" => array(
-                "Economics (English)" => 2905,
-                "Business Administration (English)" => 2905,
-                "Political Science and International Relations (English)" => 2905,
-                "Psychology (English)" => 3320
+                "Economics (English)" => array("price" => 8300, "discounted_price" => 3320),
+                "Business Administration (English)" => array("price" => 8300, "discounted_price" => 3320),
+                "Political Science and International Relations (English)" => array("price" => 8300, "discounted_price" => 3320),
+                "Psychology (English)" => array("price" => 8300, "discounted_price" => 3735)
             ),
             "FACULTY OF LAW" => array(
-                "Law School" => 3320
+                "Law School" => array("price" => 8300, "discounted_price" => 3735)
             ),
             "FACULTY OF HEALTH SCIENCES" => array(
-                "Nursing (Turkish)" => 2905,
-                "Physiotherapy and Rehabilitation (Turkish)" => 2905,
-                "Midwifery (Turkish)" => 2905,
-                "Nutrition and Dietetics (Turkish)" => 2905
+                "Nursing (Turkish)" => array("price" => 8300, "discounted_price" => 3320),
+                "Physiotherapy and Rehabilitation (Turkish)" => array("price" => 8300, "discounted_price" => 3320),
+                "Midwifery (Turkish)" => array("price" => 8300, "discounted_price" => 3320),
+                "Nutrition and Dietetics (Turkish)" => array("price" => 8300, "discounted_price" => 3320)
             ),
             "SCHOOL OF CIVIL AVIATION" => array(
-                "Pilotage (Turkish)" => 7055
+                "Pilotage (Turkish)" => array("price" => 8300, "discounted_price" => 8300)
             ),
             "VOCATIONAL SCHOOL" => array(
-                "Flight Dispatch Management (Turkish)" => 2080,
-                "Cookery (Turkish)" => 2080,
-                "Computer Programming (Turkish)" => 2080,
-                "Construction Technology (Turkish)" => 2080
+                "Cookery (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Computer Programming (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Construction Technology (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Court Office Services (Turkish)" => array("price" => 5200, "discounted_price" => 2340)
             ),
             "VOCATIONAL SCHOOL OF HEALTH SERVICES" => array(
-                "First and Emergency Aid (Turkish)" => 2080,
-                "Medical Laboratory Techniques (Turkish)" => 2080,
-                "Medical Imaging Techniques (Turkish)" => 2080,
-                "Physiotherapy (Turkish)" => 2080,
-                "Oral and Dental Health (Turkish)" => 2080,
-                "Anesthesia (Turkish)" => 2080,
-                "Surgery Services (Turkish)" => 2080,
-                "Dialysis (Turkish)" => 2080,
-                "Opticianry (Turkish)" => 2080
-            ),
-            "VOCATIONAL SCHOOL OF JUSTICE" => array(
-                "Justice (Turkish)" => 2080
+                "First and Emergency Aid (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Medical Laboratory Techniques (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Medical Imaging Techniques (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Physiotherapy (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Oral and Dental Health (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Anesthesia (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Surgery Services (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Dialysis (Turkish)" => array("price" => 5200, "discounted_price" => 2340),
+                "Opticianry (Turkish)" => array("price" => 5200, "discounted_price" => 2340)
             ),
             "MASTER PROGRAMS (ENGLISH)" => array(
-                "Master of Business Administration (Thesis- English)" => 4590,
-                "Master of Business Administration (Non- Thesis- English)" => 3400,
-                "Master of Global Politics and International Relations (Thesis- English)" => 4590,
-                "Master of Electrical and Computer Engineering (Thesis- English)" => 4590,
-                "Master of Civil Engineering (Thesis- English)" => 4590,
-                "Master of Cyber Security (Thesis- English)" => 4590,
-                "Master of Data Science (Non- Thesis- English)" => 3400
+                "Master of Business Administration (Thesis- English)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Business Administration (Non- Thesis- English)" => array("price" => 4000, "discounted_price" => 4000),
+                "Master of Global Politics and International Relations (Thesis- English)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Electrical and Computer Engineering (Thesis- English)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Civil Engineering (Thesis- English)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Cyber Security (Thesis- English)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Data Science (Non- Thesis- English)" => array("price" => 4000, "discounted_price" => 4000)
             ),
             "MASTER PROGRAMS (TURKISH)" => array(
-                "Master of Business Administration (Thesis- Turkish)" => 4590,
-                "Master of Business Administration (Non- Thesis- Turkish)" => 3400,
-                "Master of Civil Engineering (Thesis- Turkish)" => 4590,
-                "Master of Civil Engineering (Non- Thesis- Turkish)" => 3400,
-                "Master of Cyber Security (Thesis- Turkish)" => 4590,
-                "Master of Public Law (Thesis- Turkish)" => 4590,
-                "Master of Public Law (Non- Thesis- Turkish)" => 3400,
-                "Master of Private Law (Thesis- Turkish)" => 4590,
-                "Master of Private Law (Non- Thesis- Turkish)" => 3400,
-                "Master of Architecture (Thesis- Turkish)" => 4590,
-                "Physiotherapy and Rehabilitation (Thesis- Turkish)" => 4590,
-                "Master of Clinical Psychology (Thesis- Turkish)" => 8000,
-                "Master of Global Politics and International Relations (Non- Thesis- Turkish)" => 3400,
-                "Master of Occupational Health and Safety (Non- Thesis- Turkish)" => 3400
+                "Master of Business Administration (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Business Administration (Non- Thesis- Turkish)" => array("price" => 4000, "discounted_price" => 4000),
+                "Master of Civil Engineering (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Civil Engineering (Non- Thesis- Turkish)" => array("price" => 4000, "discounted_price" => 4000),
+                "Master of Cyber Security (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Public Law (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Public Law (Non- Thesis- Turkish)" => array("price" => 4000, "discounted_price" => 4000),
+                "Master of Private Law (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Private Law (Non- Thesis- Turkish)" => array("price" => 4000, "discounted_price" => 4000),
+                "Master of Architecture (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Physiotherapy and Rehabilitation (Thesis- Turkish)" => array("price" => 5400, "discounted_price" => 5400),
+                "Master of Clinical Psychology (Thesis- Turkish)" => array("price" => 15000, "discounted_price" => 15000),
+                "Master of Global Politics and International Relations (Non- Thesis- Turkish)" => array("price" => 4000, "discounted_price" => 4000),
+                "Master of Occupational Health and Safety (Non- Thesis- Turkish)" => array("price" => 4000, "discounted_price" => 4000)
             ),
-            "" => array(
-                'Ph.D. in Business Administration (Turkish)' => 7650
-            )
+            "" => [
+                "Ph.D. in Business Administration (Turkish)" => array("price" => 9000, "discounted_price" => 9000)
+            ]
         );
 
-        foreach ($departments as $department => $programs) {
 
-            foreach ($programs as $name => $price) {
+        foreach ($departments as $department => $programs) {
+            foreach ($programs as $name => $prices) {
                 $faculty = ucwords(strtolower($department));
                 $programName = ucwords(strtolower($name));
-
 
                 $exist = Department::query()
                     ->where('faculty', '=', $faculty)
@@ -119,7 +115,8 @@ class DepartmentsTableSeeder extends Seeder
                 Department::query()->create([
                     'faculty' => $faculty,
                     'name' => $programName,
-                    'annual_fee' => $price
+                    'annual_fee' => $prices['price'] ?? 0,
+                    'discounted_fee' => $prices['discounted_price'] ?? 0
                 ]);
             }
         }
