e0cc08b57f183ae020632b028f68148467790b3a
oguz.topcu <oguztopcu7@gmail.com>
2024-07-31 12:56:25 +0300

    update

--- app/Mail/SendOfficialLetterMail.php
+++ app/Mail/SendOfficialLetterMail.php
@@ -41,7 +41,7 @@ class SendOfficialLetterMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.official-letter',
+            view: 'emails.applications.official-letter',
         );
     }
 
